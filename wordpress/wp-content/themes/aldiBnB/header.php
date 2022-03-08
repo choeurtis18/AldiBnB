53495129a1bfed4ccc3b9e73c3f2968db2c117e8
farah-doghri <farah.doghri@gmail.com>
2022-03-08 22:23:29 +0100

    first style modifs

--- wordpress/wp-content/themes/aldiBnB/header.php
+++ wordpress/wp-content/themes/aldiBnB/header.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="wp-content/themes/aldiBnB/style.css">
 
     <?php wp_head(); ?>
 </head>
@@ -20,7 +21,7 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 10px 20px 0px 20px;
+    padding: 10px 20px;
     background-color: #EAEAEA;
   }
 
@@ -31,18 +32,30 @@
   .abnb-nav-child {
     display: flex;
     text-decoration: none;
+    align-items: center;
   }
 
   .abnb-icon {
-    color: #A47035;
+    color: #202020;
     margin-left: 10px;
+    text-transform: none;
+  }
+
+  .abnb-icon p {
+    font-size: 1.7rem;
+    font-weight: 700;
+  }
+  .abnb-icon img {
+    width: 40px;
+    margin-right: 15px;
   }
 
   .abnb-nav-link {
     list-style-type: none;
     text-decoration: none;
     text-transform: uppercase;
-    font-size : 18px;
+    font-size : 1.1rem;
+    font-weight: 500;
     color: #202020;
   }
   .abnb-nav-link a{
@@ -53,13 +66,17 @@
     color: #202020;
     font-size : 18px;
   }
+
+  .sub-menu {
+    display: flex;
+  }
 </style>
 
 <nav id="abnb-navbar">
   <a href="<?php echo home_url(); ?>">
     <div id="" class="abnb-nav-child abnb-icon">
-      <i class="fa fa-home fa-2xl" aria-hidden="true"></i>
-      <p>ALDIB'n'B</p>
+      <img src="wp-content/uploads/2022/03/logo.png" alt="AldiBnB Logo">
+      <p>AldiB'n'B</p>
     </div>
   </a>
 
