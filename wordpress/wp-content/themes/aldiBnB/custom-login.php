596cc378d9a668720330fba672fdf8dcfab32eda
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2024-04-19 19:56:35 +0200

    l

--- wordpress/wp-content/themes/aldiBnB/custom-login.php
+++ wordpress/wp-content/themes/aldiBnB/custom-login.php
@@ -3,6 +3,8 @@
 * Template Name: Custom Login Page
 */
 
+
+
 global $user_id;
 global $wpdb;
 
@@ -25,7 +27,33 @@ if(!($user_id)){
     } else{
         get_header();
         ?>
-        
+
+<style>
+
+.btn-color{
+  background-color: #0e1c36;
+  color: #fff;
+  
+}
+
+.profile-image-pic{
+  height: 200px;
+  width: 200px;
+  object-fit: cover;
+}
+
+
+
+.cardbody-color{
+  background-color: #ebf2fa;
+}
+
+a{
+  text-decoration: none;
+}
+
+</style>
+<!--         
         <form method="post">
             <p>
                 <label for="username">Nom d'utilisateur / Email </label>
@@ -39,7 +67,40 @@ if(!($user_id)){
             <p>
                 <button type="submit" name=""> Se connecter </button>
             </p>
-        </form>
+        </form> -->
+
+        <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <h2 class="text-center text-dark mt-5">Connexion :</h2>
+        <div class="card my-5">
+
+          <form class="card-body cardbody-color p-lg-5" method="post">
+
+            <div class="text-center">
+              <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile">
+            </div>
+
+            <div class="mb-3">
+              <input type="text" class="form-control" id="Username"  name="username" aria-describedby="emailHelp"
+                placeholder="Nom d'utilisateur / E-mail">
+            </div>
+            <div class="mb-3">
+              <input type="password" name="txtConfirmPassword" name="password" class="form-control" id="password" placeholder="Mot de passe">
+            </div>
+            <div class="text-center"><button type="submit" name="" class="btn btn-color px-5 mb-5 w-100">Connexion</button></div>
+            <div id="emailHelp" class="form-text text-center mb-5 text-dark">Pas encore inscrit?
+            <a href="" class="text-dark fw-bold"> Créer un compte
+            </a>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
         
         <?php
         get_footer();
